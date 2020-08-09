1a7c07d4b8c25061cd3d2c88982f73a3d1adf1fb
Muhibullah-09 <mc2840696@gmail.com>
2020-08-09 15:32:08 +0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -13,3 +13,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/hello kamali/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/hello Sir/i);
+  expect(linkElement).toBeInTheDocument();
+});
